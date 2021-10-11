1617086cefe9adb2b20d46f69726ed6b58857b05
bytehu <huzhijie89@126.com>
2021-10-11 23:48:01 +0800

    链接GitHub

---
+++ src/main/java/com/hu/kx/KxApplication.java
@@ -0,0 +1,13 @@
+package com.hu.kx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KxApplication.class, args);
+    }
+
+}
